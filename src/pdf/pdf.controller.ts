0cb152d28dc86772ee22cc1f2ad9cd2ea8f8c7b7
kimminjae981002 <145568228+kimminjae981002@users.noreply.github.com>
2025-04-05 15:12:22 +0900

    [FEAT] multer를 사용하기 위한 라이브러리 다운로드

--- src/pdf/pdf.controller.ts
+++ src/pdf/pdf.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  UseInterceptors,
+  UploadedFile,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import * as pdf from 'pdf-parse';
+import { Express } from 'express';
 
-@Controller('pdf')
-export class PdfController {}
+@Controller('pdf/parse')
+export class PdfParseController {
+  constructor() {}
+
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file')) // 'file' 필드에 PDF 파일 업로드
+  async uploadFile(@UploadedFile() file: Express.Multer.File): Promise<any> {
+    const pdfBuffer = file.buffer;
+
+    // PDF에서 텍스트 추출
+    const data = await pdf(pdfBuffer);
+    const extractedText = data.text;
+
+    // // 텍스트 임베딩 생성
+    // const embedding = await this.embeddingService.generateEmbedding(extractedText);
+
+    // // Qdrant에 벡터 저장
+    // const qdrantResponse = await this.qdrantService.upsertVector('pdf_collection', embedding, Date.now(), {
+    //     text: extractedText,
+    // });
+
+    return { success: true };
+  }
+}
